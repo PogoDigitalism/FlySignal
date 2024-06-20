d9771d5363d5e7f6368a03b68f8a4faa78009baf
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2024-06-20 18:10:57 +0200

    Update signallib.lua

--- signallib.lua
+++ signallib.lua
@@ -1,23 +1,36 @@
+local RunService = game:GetService('RunService')
+
 local Signal = {}
 Signal.__index = Signal
+
+local output = false
+if RunService:IsStudio() then
+	output = true
+end
 
 function Signal.new(name: string)
 	local self = setmetatable({}, Signal)
 	self._name = name
 	self._connections = {}
+	self._once_connections = {}
 
 	return self
 end
 
 function Signal:Fire(...)
-	warn('--> signal fire;', self._name)
+	if output then warn('--> signal fire;', self._name) end
 	for _, connection in ipairs(self._connections) do
 		task.spawn(connection, ...)
 	end
+	for _, connection in ipairs(self._once_connections) do
+		task.spawn(connection, ...)
+	end
+	table.clear(self._once_connections)
 end
 
 function Signal:Clean()
 	table.clear(self._connections)
+	table.clear(self._once_connections)
 end
 
 function Signal:Connect(func, forced_gc: Instance?)
@@ -26,7 +39,7 @@ function Signal:Connect(func, forced_gc: Instance?)
 	local disconnectFunc = function()
 		for i, connection in ipairs(self._connections) do
 			if connection == func then
-				warn('-->', tostring(func), 'disconnection for Signal', self._name)
+				if output then warn('-->', tostring(func), 'disconnection for Signal', self._name) end
 				table.remove(self._connections, i)
 				break
 			end
@@ -36,9 +49,26 @@ function Signal:Connect(func, forced_gc: Instance?)
 	return disconnectFunc
 end
 
+function Signal:Wait(duration : number?)
+	local Running = coroutine.running()
+
+	local _delay
+	if duration then
+		_delay = task.delay(duration, function(thread)
+			task.defer(thread)
+		end, Running)
+	end
+
+	self:Once(function(...)
+		if _delay then task.cancel(_delay) end
+		task.defer(Running, ...)
+	end)
+
+	return coroutine.yield()
+end
+
 function Signal:Once(func)
-	table.insert(self._connections, func)
-	
+	table.insert(self._once_connections, func)
 end
 
 return Signal
